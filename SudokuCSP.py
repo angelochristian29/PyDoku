d25405ed612c3509ffd72527e032dede2801eb3c
Bobby <66821137+BobbyTheFischer@users.noreply.github.com>
2023-04-20 11:40:04 -0700

    Updates csp algorithm

--- SudokuCSP.py
+++ SudokuCSP.py
@@ -1,8 +1,25 @@
-import queue
+#import queue
+from SudokuBoard import SudokuBoard
 
+alphabet = "ABCDEFGHI"
+class Queue():
+    def __init__(self) -> None:
+        self.list = []
+    def put(self,object):
+        self.list.append(object)
+    def get(self):
+        return self.list.pop(0)
+    def empty(self):
+        return (len(self.list)==0)
+    def __str__(self) -> str:
+        string = ""
+        for item in self.list:
+            string = string + str(item)
+        return string
 
 class Square():
-    def __init__(self,value = 0) -> None:
+    def __init__(self,name = "",value = 0) -> None:
+        self.name = name
         self.domain = [1,2,3,4,5,6,7,8,9]
         self.constraints = []
         self.value = value
@@ -32,6 +49,11 @@ class Square():
         if(self.value == 0):
             return False
         return constraint.remove(self.value)
+    def __str__(self) -> str:
+        string = self.name + "\nValue: " + str(self.value) + "\nDomain: "
+        for value in self.domain:
+            string = string + str(value) + " "
+        return string
 
 #class Constraint():
     #def __init__(self) -> None:
@@ -65,16 +87,15 @@ class Board():
         for i in range(box_row, box_row + self.base):
             for j in range(box_column, box_column + self.base):
                 if (i != row and j!= col):
-                    values.append(self.board[row][col])
+                    values.append(self.board[i][j])
         return values
     
     def arc_inference(self):
-        arc_queue = queue.Queue(maxsize=0)
+        arc_queue = Queue()
         for i in range(self.side):
             for j in range(self.side):
                 if(self.board[i][j].getValue() != 0):
                     arc_queue.put(self.board[i][j])
-
         while(not arc_queue.empty()):
             node = arc_queue.get()
             for constraint in node.get_constraints():
@@ -82,37 +103,34 @@ class Board():
                     arc_queue.put(constraint)
                     if(len(constraint.domain) == 0):
                         return False
-            return True
+        return True
 
     def initialize(self,init = None):
-            for i in range(self.side):
-                self.board.append([])
-                for j in range(self.side):
-                    if(init is None):
-                        self.board[i].append(Square())
-                    else:
-                        self.board[i].append(Square(init[i][j]))
-            for i in range(self.side):
-                for j in range(self.side):
-                    self.board[i][j].add_constraints(self.getColumn(i,j))
-                    self.board[i][j].add_constraints(self.getRow(i,j))
-                    self.board[i][j].add_constraints(self.getSquare(i,j))
+        for i in range(self.side):
+            self.board.append([])
+            for j in range(self.side):
+                if(init is None):
+                    self.board[i].append(Square(alphabet[i]+str(j+1)))
+                else:
+                    self.board[i].append(Square(alphabet[i]+str(j+1),init[i][j]))
+        for i in range(self.side):
+            for j in range(self.side):
+                self.board[i][j].add_constraints(self.getColumn(i,j))
+                self.board[i][j].add_constraints(self.getRow(i,j))
+                self.board[i][j].add_constraints(self.getSquare(i,j))
 
     
     def __str__(self) -> str:
-        string = ""
+        string = "\n"
         for i in range(self.side):
             string = string + "\n"
             for j in range(self.side):
                 string = string + str(self.board[i][j].getValue()) + " "
         return string
 
-board = Board([[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[9,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0]])
+board = Board([[0,0,3,0,2,0,6,0,0],[9,0,0,3,0,5,0,0,1],[0,0,1,8,0,6,4,0,0],[0,0,8,1,0,2,9,0,0],[7,0,0,0,0,0,0,0,8],[0,0,6,7,0,8,2,0,0],[0,0,2,6,0,9,5,0,0],[8,0,0,2,0,3,0,0,9],[0,0,5,0,1,0,3,0,0]])
 board.arc_inference()
 print(board)
-for i in range(board.side):
-    for j in range(board.side):
-        print(board.board[i][j].domain)
         
 
 
