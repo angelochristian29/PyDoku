f52b313706fbdd9498208c2afd71062f541ae83a
angelayang2018 <45446731+angelayang2018@users.noreply.github.com>
2023-04-21 13:25:16 -0700

    Add menu button functionality

--- SudokuGUI.py
+++ SudokuGUI.py
@@ -1,4 +1,5 @@
 from SudokuBoard import SudokuBoard
+import Menu
 import sys, pygame as pg
 
 buttons = {pg.K_1:1,pg.K_2:2,pg.K_3:3,pg.K_4:4,pg.K_5:5,pg.K_6:6,pg.K_7:7,pg.K_8:8,pg.K_9:9}
@@ -101,7 +102,7 @@ def reset_game():
 
 
 def go_to_menu():
-    print("menu")
+    Menu.main()
 
 
 def sudoku_loop():
