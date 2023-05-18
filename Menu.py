a555f2930ed81a45dc1b5ce66c8064f11f474ef4
Shivam013 <shivamamin4@gmail.com>
2023-05-18 01:39:55 -0700

    Fixed some menu issues

--- Menu.py
+++ Menu.py
@@ -1,88 +1,51 @@
 import sys
 import pygame
-
 import SudokuGUI
 
-# initialize pygame
 pygame.init()
 
 def main():
-    # set window properties
-    screen_width = 630
-    screen_height = 630
-    screen = pygame.display.set_mode((screen_width, screen_height))
+    screen = pygame.display.set_mode((630, 630))
     pygame.display.set_caption("Welcome to PyDoku!")
 
-    # set font properties
     font = pygame.font.SysFont("Arial", 36)
 
-    # create title label
     title_label = font.render("Welcome to PyDoku!", True, (0, 0, 0))
-    title_label_rect = title_label.get_rect(center=(screen_width / 2, 100))
+    title_label_rect = title_label.get_rect(center=(315 , 100))
 
-    # create start button with gray background
-    start_button_surf = pygame.Surface((150, 50))
-    start_button_surf.fill((128, 128, 128))
-    start_button_rect = start_button_surf.get_rect(center=(screen_width / 2, 300))
+    quit_text = font.render("Quit", True, pygame.Color("black"))
+    quit_rect = quit_text.get_rect(center=(70, 605))
 
-    # create quit button with gray background
-    quit_button_surf = pygame.Surface((100, 50))
-    quit_button_surf.fill((128, 128, 128))
-    quit_button_rect = quit_button_surf.get_rect(bottomleft=(10, screen_height - 10))
+    start_text = font.render("Start", True, pygame.Color("black"))
+    start_rect = start_text.get_rect(center=(320, 300))
 
-    # main loop
+
     while True:
-        # handle events
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if start_button_rect.collidepoint(event.pos):
+                x,y = pygame.mouse.get_pos()
+                if 250 <= x <= 400 and 275 <= y <= 325:
                     print("Start button clicked!")
                     SudokuGUI.sudoku_loop()
-                elif quit_button_rect.collidepoint(event.pos):
+                elif 0 <= x <= 150 and 580 <= y <= 630:
                     print("Quit button clicked!")
                     pygame.quit()
                     sys.exit()
 
-        # check for mouse events
-        mouse_pos = pygame.mouse.get_pos()
-        if start_button_rect.collidepoint(mouse_pos):
-            start_button_surf.fill((192, 192, 192))  # lighter gray when hovered
-        else:
-            start_button_surf.fill((128, 128, 128))  # darker gray when not hovered
 
-        if quit_button_rect.collidepoint(mouse_pos):
-            quit_button_surf.fill((192, 192, 192))  # lighter gray when hovered
-        else:
-            quit_button_surf.fill((128, 128, 128))  # darker gray when not hovered
 
-        # draw widgets
         screen.fill((255, 255, 255))
+        pygame.draw.rect(screen, pygame.Color("gray"), pygame.Rect(250, 275, 150, 50), 0)
+        pygame.draw.rect(screen, pygame.Color("gray"), pygame.Rect(0, 580, 150, 50), 0)
         screen.blit(title_label, title_label_rect)
-        screen.blit(start_button_surf, start_button_rect)
-        screen.blit(quit_button_surf, quit_button_rect)
-        screen.blit(font.render("Start", True, (0, 0, 0)), start_button_rect.move(35, 5))
-        screen.blit(font.render("Quit", True, (0, 0, 0)), quit_button_rect.move(15, 5))
+        screen.blit(start_text, start_rect)
+        screen.blit(quit_text, quit_rect)
 
-        # update display
         pygame.display.update()
 
-        # update display
-        pygame.display.update()
-
-        # check for mouse events
-        mouse_pos = pygame.mouse.get_pos()
-        if start_button_rect.collidepoint(mouse_pos):
-            start_button_surf.fill((192, 192, 192))  # lighter gray when hovered
-        else:
-            start_button_surf.fill((128, 128, 128))  # darker gray when not hovered
-
-        if quit_button_rect.collidepoint(mouse_pos):
-            quit_button_surf.fill((192, 192, 192))  # lighter gray when hovered
-        else:
-            quit_button_surf.fill((128, 128, 128))  # darker gray when not hovered
 
 
 if __name__ == "__main__":
